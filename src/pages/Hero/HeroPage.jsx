5ddbce617a2f40ab4ef46a3eee1bea5b25a8db96
febessg <fernanda-bess@hotmail.com>
2025-06-30 14:22:52 -0300

    feat(hero-carousel): carrossel responsivo com múltiplas imagens, navegação por setas sobrepostas e ícones do react-icons

--- src/pages/Hero/HeroPage.jsx
+++ src/pages/Hero/HeroPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./HeroPage.module.css";
 import HeroImages from "../../components/HeroImages/HeroImages";
+import img1 from "../../assets/images/hero-img-1.jpeg";
+import img2 from "../../assets/images/hero-img-2.jpeg";
+import img3 from "../../assets/images/hero-img-3.jpeg";
+import img4 from "../../assets/images/hero-img-4.jpeg";
+import img5 from "../../assets/images/hero-img-5.jpeg";
 
 const images = [
-  { src: require("../../assets/images/hero-image-1.jpg"), alt: "Obra 1" },
-  { src: require("../../assets/images/hero-image-2.jpg"), alt: "Obra 2" },
-  { src: require("../../assets/images/hero-image-3.jpg"), alt: "Obra 3" },
-  { src: require("../../assets/images/hero-image-4.jpg"), alt: "Obra 4" },
+  { src: img1, alt: "Obra 1" },
+  { src: img2, alt: "Obra 2" },
+  { src: img3, alt: "Obra 3" },
+  { src: img4, alt: "Obra 4" },
+  { src: img5, alt: "Obra 5" },
 ];
 
 const HeroPage = () => {
