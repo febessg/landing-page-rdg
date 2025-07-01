9ba8714c1cf2a23437173138ae4a273381ce5001
febessg <fernanda-bess@hotmail.com>
2025-07-01 09:42:56 -0300

    feat(contato-page): atualiza conteúdo e estilo da página de contato, adicionando subtítulo, informações de contato e melhorias na descrição

--- src/pages/ContatoPage/ContatoPage.jsx
+++ src/pages/ContatoPage/ContatoPage.jsx
@@ -5,12 +5,37 @@ import ContatoForm from "../../components/ContatoForm/ContatoForm";
 const ContatoPage = () => {
   return (
     <div className={styles["contato-page"]}>
-      <h1 className={styles["contato-title"]}>Fale Conosco</h1>
+      <h1 className={styles["contato-title"]}>Fale Conosco:</h1>
+      <span className={styles["contato-subtitle"]}>
+        Vamos Concretizar Seu Próximo Projeto!
+      </span>
       <p className={styles["contato-description"]}>
-        Entre em contato com a RDG Construções para solicitar um orçamento,
-        tirar dúvidas ou saber mais sobre nossos serviços. Estamos prontos para
-        atender você!
+        Sua visão está a um passo de se tornar realidade. Entre em contato com a
+        equipe da RDG Construções e descubra como podemos transformar seus
+        planos em um projeto de sucesso. Estamos prontos para ouvir suas ideias,
+        tirar suas dúvidas e apresentar as melhores soluções para suas
+        necessidades.
       </p>
+      <div className={styles["contato-info"]}>
+        <p>
+          Preencha o formulário abaixo ou utilize nossos canais de contato
+          direto:
+        </p>
+        <p>
+          <strong>Telefone:</strong> (48) 9 9999-9999
+          <br />
+          <strong>E-mail:</strong> contato@rdgconstrucoes.com.br
+        </p>
+        <p>
+          <strong>Endereço:</strong>
+          <br />
+          R JOSÉ PEDRO GIL, 136, CASA
+          <br />
+          Bairro Agronômica
+          <br />
+          Florianópolis - SC, CEP: 88025-030
+        </p>
+      </div>
       <ContatoForm />
     </div>
   );
