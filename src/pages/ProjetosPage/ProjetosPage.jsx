ff4fa6eedf6c08bbcbd037f6928f6b9b11f609b1
febessg <fernanda-bess@hotmail.com>
2025-07-01 10:02:30 -0300

    feat(projetos-page): adiciona nova página de portfólio de projetos com estrutura e estilo inicial

---
+++ src/pages/ProjetosPage/ProjetosPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./ProjetosPage.module.css";
+
+const ProjetosPage = () => {
+  return (
+    <div className={styles.projetosContainer}>
+      <h1>Portfólio de Projetos</h1>
+      <p>Confira alguns dos projetos realizados pela nossa empresa.</p>
+      {/* Adicione aqui os cards ou seções dos projetos futuramente */}
+    </div>
+  );
+};
+
+export default ProjetosPage;
